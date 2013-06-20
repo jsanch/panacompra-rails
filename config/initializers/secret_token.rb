4ef24458c2092e939b266b70886472d3c546a36c
Baconator507 <ibarria0@gmail.com>
2013-06-20 01:17:10 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panacompra::Application.config.secret_token = 'e96ca0e139eb449187aa1398159e2c52ceb89fea876e4457b3ae91fe430f766fb1ecac09c75e196b8278d7cf1149e07d19e3293aa67aab9743390bd293c3e6bd'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Panacompra::Application.routes.draw do
+  root :to => "home#index"
+end
