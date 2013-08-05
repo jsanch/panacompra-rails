3dc5794864a64c39722ec4d75f9cdd1ccc5709ab
bacon <bacon@nuke.(none)>
2013-08-05 09:29:28 -0500

    sort by acto

--- app/controllers/compras_controller.rb
+++ app/controllers/compras_controller.rb
@@ -16,7 +16,7 @@ class ComprasController < ApplicationController
     @compras = @compras.where('precio >= ?', params[:price_min]) if (params[:price_min] and params[:price_min] != '')
     @compras = @compras.where('precio <= ?', params[:price_max]) if (params[:price_max] and params[:price_max] != '')
     @compras = @compras.where('entidad = ?', params[:entidad]) if (params[:entidad] and params[:entidad] != '')
-    @compras = @compras.paginate(page: params[:page]).order('ACTO DESC')
+    @compras = @compras.paginate(page: params[:page]).sort_by {|compra| compra.acto.gsub(/\D/, '')}
     @entidades = Compra.select("DISTINCT(ENTIDAD)").map{|x| x.entidad}.sort
 
     respond_to do |format|
