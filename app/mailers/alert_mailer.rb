729cf9abce9438fce88adb36992166d5e3868d25
bacon <bacon@nuke.(none)>
2013-08-05 10:42:05 -0500

    more info in mail

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -2,6 +2,7 @@ class AlertMailer < ActionMailer::Base
   default from: "panacompra.herokuapp.com"
 
   def compra_alert(compra)
+    @compra = compra
     mail(:to => 'ibarria0@gmail.com', :subject => "Compra Alert")
   end
 end
