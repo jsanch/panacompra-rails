5fe93c055a2b928c57535fdf729acfb743cc0f14
Baconator507 <ibarria0@gmail.com>
2013-08-09 15:09:04 -0400

    modified search to include acto

---
+++ db/migrate/20130809190252_add_acto_to_tsv_description.rb
@@ -0,0 +1,19 @@
+class AddActoToTsvDescription < ActiveRecord::Migration
+  def up
+    execute "drop index compras_description"
+    execute "drop trigger tsvectorupdate on compras"
+    execute "UPDATE compras SET tsv_description = (to_tsvector('spanish', coalesce(description, '')) || to_tsvector('spanish', coalesce(proponente, '')) || to_tsvector('spanish',coalesce(acto,'')) )"
+    execute <<-SQL
+      CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
+      ON compras FOR EACH ROW EXECUTE PROCEDURE
+      tsvector_update_trigger(tsv_description, 'pg_catalog.spanish', description, proponente);
+    SQL
+    execute "create index compras_description on compras using gin(tsv_description)"
+  end
+
+  def down
+    execute "drop index compras_description"
+    execute "drop trigger tsvectorupdate on compras"
+  end 
+
+end
