557e272e6d880c40adb411e2aef8305562374558
Baconator507 <ibarria0@gmail.com>
2013-10-06 00:20:51 -0400

    scraper integration

--- db/migrate/20131006041800_add_scraper_fields_to_compras.rb
+++ db/migrate/20131006041800_add_scraper_fields_to_compras.rb
@@ -1,7 +1,7 @@
 class AddScraperFieldsToCompras < ActiveRecord::Migration
   def change
-    add_column :compras, :parsed, :boolean
-    add_column :compras, :visited, :boolean
+    add_column :compras, :parsed, :boolean, default: false
+    add_column :compras, :visited, :boolean, default: false
     add_column :compras, :html, :text
   end
 end
